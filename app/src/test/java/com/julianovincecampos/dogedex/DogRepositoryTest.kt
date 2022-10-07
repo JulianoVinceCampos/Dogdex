2bdc59c14c164da96eca3ec364ccba0b535a9ef7
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2022-10-07 08:07:13 -0300

    Creating a DogRepositoryTest test class and testing the DogRepository class

--- app/src/test/java/com/julianovincecampos/dogedex/DogRepositoryTest.kt
+++ app/src/test/java/com/julianovincecampos/dogedex/DogRepositoryTest.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.Test
 import org.junit.Assert.*
+import java.net.UnknownHostException
 
 class DogRepositoryTest {
 
@@ -101,6 +102,178 @@ class DogRepositoryTest {
         val dogCollection = (apiResponseStatus as ApiResponseStatus.Success).data
         assertEquals(2, dogCollection.size)
         assertEquals("fakenamedogs", dogCollection[1].nameDog)
+        assertEquals("", dogCollection[0].nameDog)
 
     }
+
+    @Test
+    fun testGetDogCollectionError() = runBlocking {
+        class FakeApiService : ApiService {
+            override suspend fun getAllDogs(): DogListApiResponse {
+                throw UnknownHostException()
+            }
+
+            override suspend fun singUp(signUpDTO: SignUpDTO): AuthApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun singIn(signUpDTO: SignInDTO): AuthApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun addDogToUser(addDogToUserDTO: AddDogTouserDTO): DefaultResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun getUserDogs(): DogListApiResponse {
+                return DogListApiResponse(
+                    message = "", isSuccess = true, DogListResponse(
+                        listOf(
+                            DogDTO(
+                                20, 20,
+                                "fakenamedogs",
+                                "",
+                                "",
+                                "",
+                                "",
+                                "",
+                                "",
+                                "",
+                                ""
+                            )
+                        )
+                    )
+                )
+            }
+
+            override suspend fun getDogByMlId(mlId: String): DogApiResponse {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        val dogRepository = DogRespository(
+            apiService = FakeApiService(),
+            dispatcher = TestCoroutineDispatcher()
+        )
+
+        val apiResponseStatus = dogRepository.getDogsCollection()
+        assert(apiResponseStatus is ApiResponseStatus.Error)
+        assertEquals(
+            R.string.unknown_host_exception,
+            (apiResponseStatus as ApiResponseStatus.Error).messageId
+        )
+    }
+
+    @Test
+    fun getDogByMLSucess() = runBlocking {
+        val idDog = 20L
+
+        class FakeApiService : ApiService {
+            override suspend fun getAllDogs(): DogListApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun singUp(signUpDTO: SignUpDTO): AuthApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun singIn(signUpDTO: SignInDTO): AuthApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun addDogToUser(addDogToUserDTO: AddDogTouserDTO): DefaultResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun getUserDogs(): DogListApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun getDogByMlId(mlId: String): DogApiResponse {
+                return DogApiResponse(
+                    message = "",
+                    isSuccess = true,
+                    data = DogResponse(
+                        dog = DogDTO(
+                            idDog.toLong(), 20,
+                            "fakenamedogs",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""
+                        )
+                    )
+                )
+            }
+        }
+
+        val dogRepository =
+            DogRespository(apiService = FakeApiService(), dispatcher = TestCoroutineDispatcher())
+
+        val apiResponseStatus = dogRepository.getDogByMlId("2121515")
+        assert(apiResponseStatus is ApiResponseStatus.Success)
+        assertEquals(idDog, (apiResponseStatus as ApiResponseStatus.Success).data.id)
+    }
+
+
+    @Test
+    fun getDogByMLError() = runBlocking {
+        val idDog = 20L
+
+        class FakeApiService : ApiService {
+            override suspend fun getAllDogs(): DogListApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun singUp(signUpDTO: SignUpDTO): AuthApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun singIn(signUpDTO: SignInDTO): AuthApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun addDogToUser(addDogToUserDTO: AddDogTouserDTO): DefaultResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun getUserDogs(): DogListApiResponse {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun getDogByMlId(mlId: String): DogApiResponse {
+                return DogApiResponse(
+                    message = "error_getting_dog_by_ml_id",
+                    isSuccess = false,
+                    data = DogResponse(
+                        dog = DogDTO(
+                            idDog.toLong(), 20,
+                            "fakenamedogs",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""
+                        )
+                    )
+                )
+            }
+        }
+
+        val dogRepository =
+            DogRespository(apiService = FakeApiService(), dispatcher = TestCoroutineDispatcher())
+
+        val apiResponseStatus = dogRepository.getDogByMlId("2121515")
+        assert(apiResponseStatus is ApiResponseStatus.Error)
+        assertEquals(R.string.unknown_error, (apiResponseStatus as ApiResponseStatus.Error).messageId)
+    }
+
 }
