b9ed35ffd34ae12c3574d9fb004a0e0938d5c2f0
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2022-10-07 08:26:49 -0300

    Class responsible for creating a default behavior of Coroutine

--- app/src/test/java/com/julianovincecampos/dogedex/viewmodel/MainCoroutineRule.kt
+++ app/src/test/java/com/julianovincecampos/dogedex/viewmodel/MainCoroutineRule.kt
@@ -6,22 +6,6 @@ import kotlinx.coroutines.test.*
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
-//@ExperimentalCoroutinesApi
-//class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) :
-//    TestWatcher(),
-//    TestCoroutineScope by TestCoroutineScope(dispatcher) {
-//    override fun starting(description: Description?) {
-//        super.starting(description)
-//        Dispatchers.setMain(dispatcher)
-//    }
-//
-//    override fun finished(description: Description?) {
-//        super.finished(description)
-//        cleanupTestCoroutines()
-//        Dispatchers.resetMain()
-//    }
-//}
-
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(
     val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
